dd9e2033fd1ec2d02faf6a37843d509abd9db0fe
Nekith <nekith@errant-works.com>
2016-11-01 17:01:34 +0100

    fix travis

---
+++ quality/tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+define('STRAY_VERSION', '0.4');
+define('STRAY_VERSION_CODE', 'Marius');
+
+define('STRAY_PATH_ROOT', __DIR__ . '/../../');
+define('STRAY_PATH_APPS', __DIR__ . '/../../apps/');
+define('STRAY_PATH_VENDOR', __DIR__ . '/../../vendor/');
+
+if (false === defined('STRAY_ENV')) {
+    define('STRAY_ENV', (getenv('STRAY_ENV') === 'development' ? 'development' : 'production'));
+}
+
+require STRAY_PATH_VENDOR . 'autoload.php';
+
+use RocknRoot\StrayFw\Bootstrap;
+use RocknRoot\StrayFw\Console\Console;
+
+Bootstrap::init();
